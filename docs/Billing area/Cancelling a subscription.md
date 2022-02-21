68a6f594a528038b767bbc6aa7953ab34fa361a3
Liam McDonald <42664720+LiamM-Dev@users.noreply.github.com>
2022-02-21 00:27:56 -0500

    Cancelling subscription
    - Added Cancelling subscriptions

---
+++ docs/Billing area/Cancelling a subscription.md
@@ -0,0 +1,41 @@
+---
+id: Cancel sub
+title: Cancelling a subsciption
+description: How to cancel your server subscription
+---
+
+### Identifying the type of subscription
+
+To identify the type of subscription you'll need to sign in to your [billing area](https://billing.aurorahosts.com) account. Once singed in click services, click on your service then on the side you'll see `Payment Method` if it shows PayPal click [here](#cancelling-a-paypal-subscription) if it shows Debit/Credit click [here](#cancelling-a-stripe-subsciption)
+
+![Payment method](../../images/Billing/Cancellation/1_productinfo.png)
+
+### Cancelling a PayPal subscription
+
+1. Sign into your PayPal account
+
+2. Click the gear icon at the top right.
+
+3. Select Payments
+
+4. Click Manage pre-approved payments
+
+5. You'll see Aurorahosts as a merchant, click it and you'll see an option to cancel. Once cancelled you'll receive an email from PayPal.
+
+### Cancelling a stripe subsciption
+
+1. Sign in to the [billing area](https://billing.aurorahosts.com)
+
+2. Click on your name at the top right
+
+![Name](../../images/Billing/Cancellation/2_home.png)
+
+3. Select Payment methods
+
+![Payment methods](../../images/Billing/Cancellation/3_payment.png)
+
+4. Find your card and click the delete icon.
+
+![Delete card](../../images/Billing/Cancellation/4_delcard.png)
+
+5. Confirm the deletion
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'AuroraHosts docs',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://docs.aurorahosts.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'AuroraHosts', // Usually your GitHub org/user name.
+  projectName: 'AuroraHosts Documentation', // Usually your repo name.
 
   presets: [
     [
@@ -23,14 +23,12 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/AuroraHosts/public-docs',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/AuroraHosts/public-docs',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -43,7 +41,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Aurorahosts docs',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -62,51 +60,6 @@ const config = {
             position: 'right',
           },
         ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
